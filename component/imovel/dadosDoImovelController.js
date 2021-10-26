9263cd16b0645abd15495c57372142f210d695e6
Monique Rodrigues Oliveira <mroolive@everis.com>
2021-10-26 18:28:15 -0300

    campos em validacao.

--- component/imovel/dadosDoImovelController.js
+++ component/imovel/dadosDoImovelController.js
@@ -1,7 +1,21 @@
-simuladorDeFinanciamentos.controller("dadosDoImovelController", function ($location,$scope, $http) {
-    $scope.titulo = "oi";
+simuladorDeFinanciamentos.controller("dadosDoImovelController", function ($location, $scope, $http) {
+    // $scope.titulo = "teste";
 
     
+    $scope.imovel = {
+        tipo:"" ,
+        renda:"",
+        valorImovel: "" ,
+        valorEntrada:"",
+        quantidParcelas:""
+    };
+
+   
+
+    
+
+
+
     //ir para tela Aprovado:
     $scope.telaAprovado = function () {
         $location.path("/aprovado")
@@ -11,13 +25,42 @@ simuladorDeFinanciamentos.controller("dadosDoImovelController", function ($locat
     //ir para tela Reprovado:
     $scope.telaReprovado = function () {
         $location.path("/reprovado")
-    };   
-   
+    };
+
 
     $scope.telaProponente = function () {
         $location.path("/proponente")
     };
 
+
+
+    //get para pegar os dados tipo de imovel e listar na tela:
+    $http.get("http://localhost:3000/tipoDeImovel")
+        .then(function (response) {
+            $scope.tipo = response.data;
+        })
+
+
+
+    // cadastrar dados do imovel e enviar para o JsonServer:
+    $scope.cadastra_imovel = function (isValid) {
+        console.log("chamou")
+        if (isValid) {
+            console.log("envio de dados ok")
+            $http.post(
+                "http://localhost:3000/imovel",
+                JSON.stringify($scope.imovel)        
+            ).then(function(response){
+                $scope.imovel.push(response.data);           
+
+
+            });
+
+        } else{
+            console.log("dados inválidos")
+        }
+
+    };
 
 
 
@@ -26,8 +69,7 @@ simuladorDeFinanciamentos.controller("dadosDoImovelController", function ($locat
 
 
 
-    
 
-    
+
 });
 
